addf4422b4ab0967ea288d83376accb3b829d224
Ilya Manyakin <101644573+moscow-intelligent@users.noreply.github.com>
2023-09-21 09:49:44 +0300

    Added registration and user removal

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
+from datetime import date
+
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Base, User, Booking
+
+DATABASE_URL = 'localhost:5432'
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+
+def recreate_database():
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+
+recreate_database()
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "API online"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/register")
+def create_user(name: str, password: str):
+    session = Session()
+    user = User(username=name, password=password, created_at=date.today())
+    session.add(user)
+    session.commit()
+    session.close()
+
+    return JSONResponse(
+        status_code=200, content={"status_code": 200, "message": "user registered"}
+    )
+
+
+@app.delete("/delete/{user_id}")
+def delete_user(user_id: int):
+    session = Session()
+    user = session.query(User).get(user_id)
+    if not user:
+        return JSONResponse(
+            status_code=404, content={"status_code": 404, "message": "user not found"}
+        )
+    session.delete(user)
+    session.commit()
+    session.close()
+
+
+@app.exception_handler(Exception)
+def exception_handler(request, exc):
+    json_resp = get_default_error_response()
+    return json_resp
+
+
+def get_default_error_response(status_code=500, message="Internal Server Error"):
+    return JSONResponse(
+        status_code=status_code,
+        content={"status_code": status_code, "message": message},
+    )
---
+++ models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    created_at = Column(Date)
+    updated_at = Column(Date)
+
+class Booking(Base):
+    __tablename__ = "bookings"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    start_time = Column(Date)
+    end_time = Column(Date)
+    comment = Column(String)
